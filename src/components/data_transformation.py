9f849658cc3b2f1d8657deb54fa774495e739cb7
AyaanAhmed2002 <ayaanahmed0100@gmail.com>
2025-09-29 22:13:41 +0530

    Prediction Pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -33,8 +33,8 @@ class DataTransformation:
             )
             cat_pipeline = Pipeline(
                 steps = [
-                    ('imputer', SimpleImputer(strategy = 'most_frequent')),
-                    ('one_hot_encoder', OneHotEncoder()),
+                    ('imputer', SimpleImputer(strategy = 'most_frequent', fill_value = 'unknown')),
+                    ('one_hot_encoder', OneHotEncoder(handle_unknown = 'ignore')),
                     ('scaler', StandardScaler(with_mean = False))
                 ]
             )
@@ -74,7 +74,7 @@ class DataTransformation:
             input_feature_test_df = test_df.drop(columns = [target_column_name], axis = 1)
             target_feature_test_df = test_df[target_column_name]
             
-            logging.info(f"Applying preprocessing object on training dataframe and testing dataframe")
+            logging.info("Applying preprocessing object on training dataframe and testing dataframe")
 
             input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
             
